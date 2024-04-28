12b153fbf44e7dd7138d24cc93fdb9cc17081dd6
sandip-36 <73772243+sandip-36@users.noreply.github.com>
2024-04-28 05:05:17 +0000

    change name

---
+++ 02_control_flow/while-and-do-while-loops.js
@@ -0,0 +1,34 @@
+// while loop
+index = 0;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["Spiderman", "Thor", "Ironman"];
+
+let i = 0;
+while (i < myArray.length) {
+    // console.log(`the value of array ${myArray[i]}`);
+    i = i + 1
+}
+
+//do while loop
+
+let score = 15
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
+
+
+
+
+
+
+
+
+
+
